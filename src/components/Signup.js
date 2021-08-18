e26f69a29131b422bfcdec3978174063e9c9fec9
Kyle Lynds <kplynds3@gmail.com>
2021-08-17 17:57:58 -0700

    react in scope

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,6 @@
-import { useState } from "react";
+import React, { useState } from "react";
 // import { connect } from "react-redux";
 // import { useHistory } from "react-router-dom";
-
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import theme from "../theme";
